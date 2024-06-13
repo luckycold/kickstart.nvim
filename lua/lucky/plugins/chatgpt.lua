7ffd2e9cce482abdd02b53cdf2a8fbf73f177d77
Luke <git@1al.cc>
2024-06-13 16:57:57 -0500

    Fixed chatGPT passkey for bitwarden

--- lua/lucky/plugins/chatgpt.lua
+++ lua/lucky/plugins/chatgpt.lua
@@ -3,7 +3,7 @@ return {
   event = 'VeryLazy',
   config = function()
     require('chatgpt').setup {
-      api_key_cmd = 'bw get item d17938ee-48ae-4538-829d-64f5bde45129 | jq -r \'.fields[] | select(.name == "Personal API Key") | .value\' -r',
+      api_key_cmd = 'echo $(bw get item d17938ee-48ae-4538-829d-64f5bde45129 | jq -r \'.fields[] | select(.name == "Personal API Key") | .value\' -r)',
     }
   end,
   dependencies = {
